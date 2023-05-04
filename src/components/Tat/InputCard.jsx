90fc87dda8e106fed5ee5d44a8b18a2a5c1bfabb
Sonam Sharma <akshu199bhardwaj#gmail.com>
2023-05-05 01:32:00 +0530

    `

--- src/components/Tat/InputCard.jsx
+++ src/components/Tat/InputCard.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
+import React from "react";
 
-function InputCard({setStoryInput,img_no, storyInput}) {
+function InputCard({ setStoryInput, img_no, storyInput }) {
   return (
-    <div className='container'>
-    <div className="carousel-item active">
-              <textarea
-                id={`tatinput`+img_no}
-                name="tatinput"
-                value={storyInput.s0}
-                rows="8"
-                cols="70"
-                onChange={(e) => setStoryInput({s0:e.target.value})}
-              ></textarea>
-              <div className="text-center mt-5">{`Story ${img_no}`}</div>
+    <div className="container">
+      <div className="carousel-item active">
+        <textarea
+          id={`tatinput` + img_no}
+          name={`s${img_no}`}
+          value={"storyInput"}
+          rows="8"
+          cols="70"
+          onChange={(e) => {
+            console.log(e.target.name, e.target.value);
+            // setStoryInput({
+            //   ...storyInput,
+            //   [e.target.name]: e.target.value,
+            // });
+          }}
+        ></textarea>
+        <div className="text-center mt-5">{`Story ${img_no}`}</div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default InputCard
+export default InputCard;
