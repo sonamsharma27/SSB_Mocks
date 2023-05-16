e3e93674a0ce1d2510ca00e4754e2ae469f51869
Sonam Sharma <akshu199bhardwaj#gmail.com>
2023-05-17 00:16:14 +0530

    UI completed for content adding section. UI yet to be improved

--- src/components/TestMaterialByAspirants/Test_material_aspirant.jsx
+++ src/components/TestMaterialByAspirants/Test_material_aspirant.jsx
@@ -5,10 +5,10 @@ function Test_material_aspirant() {
   const [word, setWord] = useState("");
   const [srt, setSrt] = useState("");
   const [tat, setTat] = useState("");
-  const [url, setUrl] = useState("");
+  const [urlPpdt, setUrlPpdt] = useState("");
   const [gpe, setGpe] = useState({
-    url:'',
-    problem: ''
+    url: "",
+    problem: "",
   });
   return (
     <>
@@ -53,18 +53,113 @@ function Test_material_aspirant() {
             Add SRT content
           </div>
         </div>
-        <div className={`${active === "wat" ? "mt-4 bg-light border border-2 border-dark p-3" : "d-none"}`}>
-          
-            <h5>
-              Instructions
-            </h5>
-              <li className="m-0">Add one word at a time.</li>
-              <li className="m-0">Avoid adding repeated words.</li>
-              <div className="d-flex mt-3 ">
-              <input className="p-2 rounded-2 w-30 mx-2" placeholder="Add your word here"value={word} onChange={e=>setWord(e.target.value)}/>
+        <div
+          className={`${
+            active === "wat"
+              ? "mt-4 bg-light border border-2 border-dark p-3"
+              : "d-none"
+          }`}
+        >
+          <h5>Instructions</h5>
+          <li className="m-0">Add one word at a time.</li>
+          <li className="m-0">Avoid adding repeated words.</li>
+          <div className="d-flex mt-3 ">
+            <input
+              className="p-2 rounded-2 w-30 mx-2"
+              placeholder="Add your word here"
+              value={word}
+              onChange={(e) => setWord(e.target.value)}
+            />
             <div className="btn btn-info w-20">Add</div>
-              </div>
-            
+          </div>
+        </div>
+        <div
+          className={`${
+            active === "ppdt"
+              ? "mt-4 bg-light border border-2 border-dark p-3"
+              : "d-none"
+          }`}
+        >
+          <h5>Instructions</h5>
+          <li className="m-0">Add url of the image for PPDT.</li>
+          <li className="m-0">Avoid adding repeated urls.</li>
+          <div className="d-flex mt-3 ">
+            <input
+              className="p-2 rounded-2 w-30 mx-2 ppdtcontent"
+              placeholder="Add your url here"
+              value={urlPpdt}
+              onChange={(e) => setUrlPpdt(e.target.value)}
+            />
+            <div className="btn btn-info w-20">Add</div>
+          </div>
+        </div>
+        <div
+          className={`${
+            active === "tat"
+              ? "mt-4 bg-light border border-2 border-dark p-3"
+              : "d-none"
+          }`}
+        >
+          <h5>Instructions</h5>
+          <li className="m-0">Add url of the image for tat.</li>
+          <li className="m-0">Avoid adding repeated urls.</li>
+          <div className="d-flex mt-3 ">
+            <input
+              className="p-2 rounded-2 w-30 mx-2 ppdtcontent"
+              placeholder="Add your url here"
+              value={tat}
+              onChange={(e) => setTat(e.target.value)}
+            />
+            <div className="btn btn-info w-20">Add</div>
+          </div>
+        </div>
+        <div
+          className={`${
+            active === "srt"
+              ? "mt-4 bg-light border border-2 border-dark p-3"
+              : "d-none"
+          }`}
+        >
+          <h5>Instructions</h5>
+          <li className="m-0">Add situation for SRT.</li>
+          <li className="m-0">Avoid adding same content.</li>
+          <div className="d-flex mt-3 ">
+            <input
+              className="p-2 rounded-2 w-30 mx-2 ppdtcontent"
+              placeholder="Add your url here"
+              value={srt}
+              onChange={(e) => setSrt(e.target.value)}
+            />
+            <div className="btn btn-info ">Add</div>
+          </div>
+        </div>
+        <div
+          className={`${
+            active === "gpe"
+              ? "mt-4 bg-light border border-2 border-dark p-3"
+              : "d-none"
+          }`}
+        >
+          <h5>Instructions</h5>
+          <li className="m-0">Add url of the GPE model and problem statement for GPE.</li>
+          <li className="m-0">Avoid adding same content.</li>
+          <div className="d-flex mt-3 flex-column ">
+            <input
+              className="p-2 rounded-2 w-30 mx-2 my-2 ppdtcontent"
+              placeholder="Add your url here"
+              value={gpe.url}
+              onChange={(e) => setGpe({url: e.target.value,problem:gpe.problem})}
+            />
+             <textarea
+              className="p-2 rounded-2 w-30 mx-2 ppdtcontent"
+              rows="10"
+              cols="8"
+              placeholder="Add your problem  statement here"
+              value={gpe.url}
+              onChange={(e) => setGpe({url: gpe.url,problem:e.target.value})}
+            /><br/>
+            <div className="btn btn-info w-sm mx-2">Add</div>
+          </div>
         </div>
       </div>
     </>
